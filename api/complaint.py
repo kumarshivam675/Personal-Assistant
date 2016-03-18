44db0c47042a1f5973f0273b041073e5b2bbb0fe
sammme <samyak.upadhyay123@gmail.com>
2016-03-18 16:40:49 +0530

    added complaints

---
+++ api/complaint.py
@@ -0,0 +1,10 @@
+import csv
+
+
+def complaintLodge(problem,lat, long, url, caption):
+    hello = [[problem,lat,long,url,caption]]
+
+    print lat, long, url, caption
+    with open('sidd/complaint.csv', 'a') as testfile:
+        csv_writer = csv.writer(testfile)
+        csv_writer.writerow(hello[0])	
